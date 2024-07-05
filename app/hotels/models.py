230541cc19a1d91866da1224ebf170a2fb277f87
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-05 21:21:27 +0300

    added models

---
+++ app/hotels/models.py
@@ -0,0 +1,26 @@
+from app.database import Base
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+
+
+class Hotels(Base):
+    __tablename__ = "hotels"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    services = Column(JSON, nullable=False)
+    rooms_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer, nullable=False)
+
+
+class Rooms(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True)
+    hotel_id = Column(ForeignKey("hotels.id"), nullable=False)
+    name = Column(String)
+    description = Column(String, nullable=True)
+    price = Column(Integer, nullable=False)
+    services = Column(JSON, nullable=True)
+    quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
