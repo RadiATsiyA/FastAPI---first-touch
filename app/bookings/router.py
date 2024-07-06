4d98169a154a420741276f377f7cbd5f4f202e4c
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-06 17:48:33 +0300

    bookings by id

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.bookings.schemas import BookingScheme
 from app.bookings.service import BookingService
-
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(
     prefix="/bookings",
@@ -11,6 +12,6 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings() -> list[BookingScheme]:
-    return await BookingService.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[BookingScheme]:
+    return await BookingService.find_all(user_id=user.id)
 
