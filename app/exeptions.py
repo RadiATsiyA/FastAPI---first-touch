c278010de6212372413ae8ab4dd8f18114bced5e
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-06 20:47:43 +0300

    exceptions

--- app/exeptions.py
+++ app/exeptions.py
@@ -33,6 +33,12 @@ InvalidTokenFormatException = HTTPException(
 UserIsNotExistsException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 
+RoomCannotBeBookedException = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Room can not be booked"
+)
+
+
 
 
 
