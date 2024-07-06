f1770c41a5a47ce39b944f99c02fefe0de691da1
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-06 17:47:43 +0300

    exceptions

---
+++ app/exeptions.py
@@ -0,0 +1,41 @@
+from fastapi import status, HTTPException
+
+
+UserAlreadyExistsException = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="User already exists"
+)
+
+IncorrectEmailOrPasswordException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid email or password"
+)
+
+
+TokenExpiredException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token expired"
+)
+
+
+TokenAbsentException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token doesn't exists"
+)
+
+
+InvalidTokenFormatException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid token format"
+)
+
+
+UserIsNotExistsException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
+
+
+
