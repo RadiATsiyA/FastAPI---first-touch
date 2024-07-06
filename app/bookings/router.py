7b99a276cc4e186d04a36c3a8665bf79b9331a21
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-06 13:35:32 +0300

    moved buisness-logic from controller

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from sqlalchemy import select
 
-from app.database import async_session_maker
-from app.bookings.models import Bookings
+from app.bookings.schemas import BookingScheme
+from app.bookings.service import BookingService
 
 
 router = APIRouter(
@@ -12,9 +11,6 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings():
-    async with async_session_maker() as session:
-        query = select(Bookings)
-        result = await session.execute(query)
-        return result.scalars().all()
+async def get_bookings() -> list[BookingScheme]:
+    return await BookingService.find_all()
 
