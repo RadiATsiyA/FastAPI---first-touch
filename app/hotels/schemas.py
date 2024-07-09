fa6a170b2f6f0f4966e09cd659be8ce29886bab7
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-09 18:07:00 +0300

    schema update

--- app/hotels/schemas.py
+++ app/hotels/schemas.py
@@ -1,13 +1,24 @@
-from pydantic import BaseModel, Json
+from typing import List
+
+from pydantic import BaseModel
 
 
-class HotelsScheme(BaseModel):
-    id: int
+class HotelCreateUpdateScheme(BaseModel):
+    id: int | None = None
     name: str
     location: str
-    services: Json
+    services: List[str]
     rooms_quantity: int
     image_id: int
 
     class Config:
         from_attributes = True
+
+
+class HotelsGetScheme(BaseModel):
+    id: int
+    name: str
+    location: str
+    services: List[str]
+    rooms_quantity: int
+    image_id: int
