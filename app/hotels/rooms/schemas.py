26b76a8d746e368a36d9697584be38c489375ef9
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-11 22:52:38 +0300

    scheme for rooms

---
+++ app/hotels/rooms/schemas.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class RoomsScheme(BaseModel):
+    id: int
+    hotel_id: int
+    name: str
+    description: str
+    price: int
+    services: List[str]
+    quantity: int
+    image_id: int
