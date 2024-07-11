0a290d4724270cca2b66b427a665cec4a416482c
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-11 22:52:18 +0300

    added get rooms func

---
+++ app/hotels/rooms/router.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+from app.exeptions import InvalidDateRangeException
+from app.hotels.router import router
+from app.hotels.rooms.service import RoomService
+
+
+@router.get("/{hotel_id}/rooms")
+async def get_rooms(hotel_id: int, date_from: date, date_to: date):
+    if date_from > date_to:
+        raise InvalidDateRangeException
+    return await RoomService.find_all_available_rooms(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
+
