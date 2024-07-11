d3ad5c8dac0132e5fb080a0747036213494b1805
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-11 22:57:22 +0300

    a little chages

--- app/exeptions.py
+++ app/exeptions.py
@@ -56,6 +56,11 @@ HotelUpdateErrorExecption = HTTPException(
     detail="Error while updating hotel"
 )
 
+InvalidDateRangeException = HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    detail="Invalid date range"
+)
+
 
 
 
