2f511a09b46deff46004e40b02520d914a4cb9ce
Aidar Myrzageldiev <akkaunt.lewshi@list.ru>
2024-07-25 22:13:15 +0300

    relationship for admin showing

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 from sqlalchemy import Column, Integer, String
 
@@ -8,3 +10,8 @@ class Users(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
     password = Column(String, nullable=False)
+
+    booking = relationship("Bookings", back_populates="user")
+
+    def __str__(self):
+        return f"User {self.email}"
